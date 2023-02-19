3dd95d67e142df775bdde89f0cc18c138dd061b3
Ankit Jha <jhaa11701@gmail.com>
2023-02-19 12:40:56 +0530

    added starter files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
     extend: {},
     fontFamily: {
       mono: ['Space Mono', 'monospace']
+    },
+    colors: {
+      white: '#ffff',
+      lightPrimary: '#F7F8FE',
+      darkBlue: '#0079FF',
+      black: 'black',
+      darkPrimary: '#151C2E',
+      darkSecondary: '#1F2B47'
     }
   },
   plugins: [],
